2c2302c096118a13a00c26f7d298f47b7360bc71
TsatsynAV <tsatsynav@icloud.com>
2022-01-15 11:43:50 +0300

    module 3 - completed (2 solutions looked)

--- 3-module/1-task/index.js
+++ 3-module/1-task/index.js
@@ -1,3 +1,15 @@
+console.clear()
+
+let vasya = { name: 'Вася', age: 25 };
+let petya = { name: 'Петя', age: 30 };
+let masha = { name: 'Маша', age: 28 };
+
+let users = [ vasya, petya, masha ];
+
 function namify(users) {
-  // ваш код...
+  let newArr = [];
+  users.map((getName) => newArr.push(getName.name));
+  return newArr;
 }
+
+let names = namify(users);
--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -1,3 +1,24 @@
+let arr = [5, 3, 8, 1];
+
 function filterRange(arr, a, b) {
-  // ваш код...
+  return arr.filter((i) => a <= i && i <= b );
 }
+
+let filtered = filterRange(arr, 1, 4);
+
+console.log( filtered ); // [3,1] (совпадающие значения)
+console.log( arr ); // [5,3,8,1] (без изменений)
+
+
+//TODO не работает развернутая функция (не стрелочная)
+//TODO Прошу объяснить написание
+
+
+/*
+function filterRange(arr, a, b) {
+
+  let i = arr.filter(function() {
+  return a <= i && i <= b;
+  });
+};
+*/
